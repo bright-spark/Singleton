da6383bc3b0a85a4f3101e7468aecc91bd2ef70e
fsuke1985 <fsuke1985@gmail.com>
2016-01-05 12:07:53 +0900

    commit

--- a.rb
+++ a.rb
@@ -1,19 +1,7 @@
 #!/usr/bin/ruby
 
-require 'net/http'
-require 'uri'
 require 'pp'
 require 'twitter';
-
-uri = URI.parse("http://localhost:9999")
-
-#make post data
-pos = Net::HTTP::Post.new(uri.to_s)
-pos.set_form_data({
-  :a => "hoge",
-  :ss => "jjj",
-})
-
 
 client = Twitter::REST::Client.new do |config|
     config.consumer_key        = "uVjFonGBh9zILPogHQ6YZltoM"
@@ -22,7 +10,16 @@ client = Twitter::REST::Client.new do |config|
     config.access_token_secret = "ddkkA1L3YuOsNbTjcz1rTHd3X7SoR2dQ2ewdFY0MkFMPq"
 end
 
-p client.TragetUser("o__sari__o")
+line_media = client.user_timeline("o__sari__o")
+
+line_media.each do |m|
+  m.media.each do |tp|
+    pp tp.media_url.to_s
+  end
+end
+
+
+
 class Aff
 
   def initialize
